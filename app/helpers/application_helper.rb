cf8a8af62809a7d7e7ecc75440b87a661ee1e563
Cara Michele <caraheacock@gmail.com>
2019-02-18 00:07:06 -0600

    Add club nav, add placeholder headers for data, clean up links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def club_nav?
+    (controller_name == "clubs" && action_name != "index") ||
+      controller_name == "meetings" ||
+      controller_name == "members"
+  end
 end
